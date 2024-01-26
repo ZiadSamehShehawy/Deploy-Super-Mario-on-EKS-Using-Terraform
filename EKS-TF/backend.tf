fca5a79b9bdab6707f8f969a5dd0e37d9cb801f0
ZiadSamehShehawy <132923981+ZiadSamehShehawy@users.noreply.github.com>
2024-01-26 15:41:27 +0200

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mario12bucket" # Replace with your actual S3 bucket name
+    bucket = "zoz-bucket-mario" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
